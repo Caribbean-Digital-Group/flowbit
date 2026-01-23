9383153465ebe0b1aa63d20340d2ce37dd844077
edsonfervo <edson55_@hotmail.com>
2026-01-23 17:06:01 -0600

    Refactor app.vue to include header and footer components, replacing the placeholder text.

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</template>
+    <header>
+      Header content
+    </header>
+    <NuxtPage />
+    <footer>
+      Footer content
+    </footer>
+  </template>
+  
---
+++ app/pages/admin/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Admin index page</h1>
+</template>
