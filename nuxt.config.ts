66ac5efe1af34b20a9095852ae242ddc59d21c16
edsonfervo <edson55_@hotmail.com>
2026-01-23 18:18:35 -0600

    Add Supabase configuration and update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabasePublishableKey: process.env.SUPABASE_PUBLISHABLE_KEY,
+    },
+  },
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
